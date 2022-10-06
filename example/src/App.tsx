f8ca88d8fcabdad85c2c1b72290cb7fd9adbcaca
Christopher Fields II <christopherfieldsii@gamestop.com>
2022-10-06 03:27:44 -0500

    feat: + very bad react example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,24 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Key, TypedStorage, JSStorage } from 'js-storage-abstraction';
+
+type StorageId = 'persistentName' | 'sessionName';
+
+class LocalStorageKey extends Key<StorageId> {
+  async get(): Promise<string | null> {
+    return localStorage.getItem(this.name);
+  }
+
+  async set(value: string): Promise<void> {
+    await localStorage.setItem(this.name, value);
+  }
+
+  async delete(): Promise<void> {
+    await localStorage.removeItem(this.name);
+  }
+}
+
+class SessionStorageKey extends Key<StorageId> {
+  async get(): Promise<string | null> {
+    return sessionStorage.getItem(this.name);
+  }
+
+  async set(value: string): Promise<void> {
+    await sessionStorage.setItem(this.name, value);
+  }
+
+  async delete(): Promise<void> {
+    await sessionStorage.removeItem(this.name);
+  }
+}
+
+const typedStorage: TypedStorage<StorageId> = {
+  persistentName: new LocalStorageKey('persistentName'),
+  sessionName: new SessionStorageKey('sessionName'),
+};
+const storage = new JSStorage(typedStorage);
 
 function App() {
+  const [persistentText, setPersistentText] = useState('');
+  const [sessionText, setSessionText] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br />
+
+      <input
+        type="text"
+        onChange={(e) => setPersistentText(e.currentTarget.value)}
+      />
+      <button
+        onClick={() => {
+          storage.use('persistentName').set(persistentText);
+          console.log('set persistent');
+        }}
+      >
+        set persistent text
+      </button>
+
+      <br />
+      <br />
+
+      <input
+        type="text"
+        onChange={(e) => setSessionText(e.currentTarget.value)}
+      />
+      <button
+        onClick={() => {
+          storage.use('sessionName').set(sessionText);
+          console.log('set session');
+        }}
+      >
+        set session text
+      </button>
+
+      <br />
+      <br />
+
+      <button onClick={storage.clear}>clear storage</button>
+
+      <br />
+      <br />
+
+      <button
+        onClick={() => {
+          storage.getJSON().then(console.log);
+        }}
+      >
+        print storage
+      </button>
     </div>
   );
 }
